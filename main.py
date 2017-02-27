755d7eb660b13056df5218a3c1bc7cddb24eddce
Linnux234 <Linnux234@users.noreply.github.com>
2017-02-28 00:17:25 +0100

    Update main.py
    Add experimental multithreading

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import smbus
 import config
 import os
 import time
-#import thread
+import threading
 
 
 bus = smbus.SMBus(1)
@@ -31,50 +31,49 @@ def setup():
 				config.set(sections[c], 'value', default)	
 				config.write()
 				print("ok")
-
+#				time.sleep(delay)
 #			print(port)
 #			print(default)
 			
 
 
-def temp():
+def temp(delay):
 	print('temp')
 	for i in range(len(sections)):
 		c = i - 1
 		device = config.get(sections[c], 'device')
 		if device == 'temp':
-			name = config.get(sections[c], 'name')
 			should = float(config.get(sections[c], 'should'))
 			target = config.get(sections[c], 'target')
 			target_address = int(config.get(sections[c], 'target_address'))
 			print(should)		
-			
-			value = config.get_float(sections[c], 'value')
+			value = tempvalue(sections[c])
+	#		value = config.get_float(sections[c], 'value')
 			print(value)
 			if value >= should:
 				print('genug')
-				value = getvalue(target)
+				value = getlist(target)
 				value[target_address] = '0'
 				
 			elif value < should:
 				print('nicht genug') 
-				value = getvalue(target)                  
+				value = getlist(target)                  
 				value[target_address] = '1'
-			print(target)
-			print(target_address)
-			print(value[target_address])
-			print(value)
+#			print(target)
+#			print(target_address)
+#			print(value[target_address])
+#			print(value)
 			set = ''.join(value)
 			config.set(target, 'should', set)
 		#	bus_write(address, port, int(set
-
+		time.sleep(delay)
 
 
      #       temfile.close()
 
 
 
-def expander():
+def expander(delay):
 	print('expander')
 	for i in range(len(sections)):
 		c = i - 1
@@ -92,9 +91,14 @@ def expander():
 					config.set(sections[c], 'value', should)
 					config.write()
 					print("Done")
-
+					time.sleep(delay)
+	
 		
-def tempvalue():
+def tempvalue(section):
+	
+	name = config.get(section, 'name')
+
+
 	
 	try:
 		tempfile = open('sys/bus/w1/devices/' + str(name) + 'w1_slave')
@@ -104,16 +108,19 @@ def tempvalue():
 			m =  re.match(r"[0-9a-f{2} ){9}t=([+-]?[0-9]+)",line)
 			if m:
 				value = float(m.group(2)) / 1000.0
-				config.set(section[c], 'value', value)
+				config.set(section, 'value', str(value))
+				return value
 	except (IOError):
 		print("File not found")
+		value = config.get_float(section, 'default')  
+		config.set(section, 'value', str(value))
+		return value
 
-
-def getvalue(section):
+def getlist(section):
 	value = config.get(section, 'value')
-	print(value)
+#	print(value)
 	bin = list(value)
-	print(bin)
+#	print(bin)
 	return bin
 
 	
@@ -143,9 +150,20 @@ def bus_read(address, port):
 	return value
 
 setup()
-temp()
-expander()
+
+while True:
+	try:
+		thread1 = (temp(2))
+		thread2 = (expander(1))
+
+		thread1.start()
+		thread2.start()
+		thread1.join()
+		thread2.join()
+	except:
+		print('none')
+#temp(2)
+#expander(1)
 #print("end")
 
 # thread.start_new_thread()
-
