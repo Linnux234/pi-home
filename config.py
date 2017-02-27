6fbc0e281a2b44bab18b55412fa3c17e29ed322f
Linnux234 <Linnux234@users.noreply.github.com>
2017-02-27 23:36:43 +0100

    Update config.py
    Add some function and new options for the config

--- config.py
+++ config.py
@@ -17,12 +17,17 @@ settings.read('test.cfg')
 #print(len(configs))
 
 def sections():
-	print(settings.sections())
+	return (settings.sections())
 
 
 
 def get(section, param):
-	print(settings.get(section, param))
+	return (settings.get(section, param))
+
+
+def get_float(section, param):
+	return (settings.getfloat(section, param))
+
 
 
 def add_section(section):
@@ -47,22 +52,38 @@ def create(section):
 	device = input('Device: ')
 	set(section, 'device', device)
 	if device == 'expander':
-		adress = input('Adress: ')
-		set(section, 'adress', adress)
+		address = input('Address: ')
+		set(section, 'address', address)
 		port = input('Port: ')
 		set(section, 'port', port)
+		mode = input('Mode: ')
+		set(section, 'mode', mode)
+
 
 		for i in range(8):
 			name = input('Name' + str(i) + ': ')
 			set(section, 'name' + str(i), name)
 	else:
+
+		address = input('Address: ')
+		set(section, 'address', address)
 		name = input('Name: ')
 		set(section, 'name', name)
+
+		target = input('Target: ')
+		set(section, 'target', target)
+
+		target_address = input('Target_Address: ')
+		set(section, 'target_address', target_address)
+
+
 
 
 	default = input('Default: ')
 	set(section, 'default', default)
-	
+
+	should = input('Should: ')
+	set(section, 'should', should)	
 	set(section, 'value', '')
 
 
@@ -74,9 +95,9 @@ if len(args) > 1:
 		add_section(args[2])
 		write()	
 	elif args[1] == '-list':
-		sections()
+		print(sections())
 	elif args[1] == '-get':
-		get(args[2], args[3])
+		print(get(args[2], args[3]))
 	elif args[1] == '-set':
 		set(args[2], args[3], args[4])
 		write()
@@ -86,6 +107,9 @@ if len(args) > 1:
 	elif args[1] == '-create':
 		create(args[2])
 		write()
+	elif args[1] == '-get_float':
+		print(get_float(args[2], args[3]))
+
 
 #print(args)
 #print(args[1])
